e3f267150ec4edcd35263d089912df6d01e29bf3
Mars Pinho <marciopinho@gmail.com>
2022-12-15 22:08:12 -0300

    css comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+// _app.tsx is a Next.js file that allows overriding and extending the default App component.
+// it is the main entry point for all pages in the application.
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
